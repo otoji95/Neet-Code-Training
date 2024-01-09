124480ff396a46c0721c9d556763dc230bf58e76
Michael Tanaka <michaelotanaka@gmail.com>
2024-01-08 23:00:54 -0800

    adds test_two_sum.py; adjusts stats to be for worst case scenarios for algorithims

--- tests/test_contains_duplicates.py
+++ tests/test_contains_duplicates.py
@@ -1,7 +1,7 @@
 from array_and_hashings.contains_duplicates.contains_duplicates_after_neetcode import (
     contains_duplicates,
 )
-from memory_profiler import profile, memory_usage
+from memory_profiler import memory_usage
 import time
 
 
@@ -18,35 +18,20 @@ def test_large_list_no_duplicates():
     assert contains_duplicates(nums) == False
 
 
-def test_large_list_with_duplicates_stats():
-    nums = list(range(5000)) + list(range(5000))
+def test_worst_case_no_dupes():
+    nums = list(range(9999))
+    nums.append(9999)
+    nums.append(10000)
 
-    # Time profiling
+    # Time Profiling
     start_time = time.time()
     result = contains_duplicates(nums)
     end_time = time.time()
-    elapsed_time = (end_time - start_time) * 1000
-    print(f"\n\nApproximate Time used: {elapsed_time:.2f} ms.")
+    elapsed_time_ms = (end_time - start_time) * 1000
+    print(f"\n\n Approximate time: {elapsed_time_ms:.2f} ms")
 
-    # Memory profiling
+    # Memory Profiling
     mem_usage = memory_usage((contains_duplicates, (nums,)))
-    print(f"\nPeak memory usage: {max(mem_usage):.2f} MB\n")
-
-    assert result == True
-
-
-def test_large_list_no_duplicates_stats():
-    nums = list(range(10000))
-
-    # Time profiling
-    start_time = time.time()
-    result = contains_duplicates(nums)
-    end_time = time.time()
-    elapsed_time = (end_time - start_time) * 1000
-    print(f"\n\nApproximate Time used: {elapsed_time:.2f} ms.")
-
-    # Memory profiling
-    mem_usage = memory_usage((contains_duplicates, (nums,)))
-    print(f"\nPeak memory usage: {max(mem_usage):.2f} MB\n")
+    print(f"\nPeak memory usage: {max(mem_usage):.2f} MB \n")
 
     assert result == False
---
+++ tests/test_two_sum.py
@@ -0,0 +1,31 @@
+from array_and_hashings.two_sum.two_sum_after_neetcode import two_sum
+from memory_profiler import memory_usage
+import random
+import time
+
+
+def test_two_sum_simple():
+    assert two_sum([2, 7, 11, 15], 9) == [0, 1]
+
+
+def test_repeating_elements():
+    assert two_sum([3, 3], 6) == [0, 1]
+
+
+def test_two_sum_random_time_and_memory():
+    nums = range(10001)
+
+    # Time Management
+    start_time = time.time()
+    result = two_sum(nums, 19999)
+    end_time = time.time()
+
+    elapsed_time_ms = (end_time - start_time) * 1000
+
+    print(f"\n\nApproximate time: {elapsed_time_ms:.2f} ms")
+
+    # Memory Profiling
+    mem_usage = memory_usage((two_sum, (nums, 19999)))
+    print(f"\nPeak memory usage: {max(mem_usage):.2f} MB \n")
+
+    assert result == [9999, 10000]
